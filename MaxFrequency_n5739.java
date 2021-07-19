7ffb104f298e341800545da8107f76f239c61456
panhao <phnobugs@gmail.com>
2021-07-19 12:42:19 +0800

    更新每日一题

--- MaxFrequency_n5739.java
+++ MaxFrequency_n5739.java
@@ -9,23 +9,22 @@ import java.util.Arrays;
 public class MaxFrequency_n5739 {
     public int maxFrequency(int[] nums, int k) {
         Arrays.sort(nums);
-        int n=nums.length;
-        int l=0, ans =1,total=0;
-        for(int i=1;i<n;i++){
-            total+=(nums[i]-nums[i-1])*(i-l);
-            while(total>k){
-                total-=nums[i]-nums[l];
+        int n = nums.length;
+        int l = 0, ans = 1, total = 0;
+        for (int i = 1; i < n; i++) {
+            total += (nums[i] - nums[i - 1]) * (i - l);
+            while (total > k) {
+                total -= nums[i] - nums[l];
                 l++;
             }
-            ans =Math.max(ans,i-l+1);
+            ans = Math.max(ans, i - l + 1);
         }
         return ans;
-
     }
 
     public static void main(String[] args) {
-        MaxFrequency_n5739 m=new MaxFrequency_n5739();
-        int[] nums={1,2,4,6};
-        System.out.println(m.maxFrequency(nums,5));
+        MaxFrequency_n5739 m = new MaxFrequency_n5739();
+        int[] nums = {1, 2, 4, 6};
+        System.out.println(m.maxFrequency(nums, 5));
     }
 }
