82157379cd114836ef7a312c141a0ce76bceef23
panhao <phnobugs@gmail.com>
2021-07-25 12:51:24 +0800

    更新双周赛，周赛

--- MaxCompatibilitySum_n5825.java
+++ MaxCompatibilitySum_n5825.java
@@ -1,9 +1,5 @@
 package leetcode;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashSet;
-
 /**
  * @program: first
  * @description:
@@ -11,46 +7,51 @@ import java.util.HashSet;
  * @date: 2021-07-25 10:56
  **/
 public class MaxCompatibilitySum_n5825 {
-    public static int maxCompatibilitySum(int[][] students, int[][] mentors) {
-        HashSet<Integer> set=new HashSet<>();
-        for (int i=0;i<mentors.length;i++){
-            set.add(i);
-        }
-        int ans=0;
-        for (int i=0;i<students.length;i++){
-            int max_sum=0;
-            int max_cur=-1;
-            for (int j=0;j<mentors.length;j++){
-                if (!set.contains(j)) continue;
-                int sum=0;
-                for (int k=0;k<mentors[j].length;k++){
-                    if (students[i][k]==mentors[j][k]) sum++;
-                }
-                if (sum>max_sum){
-                    max_sum=sum;
-                    max_cur=j;
-                }
-            }
-            ans+=max_sum;
-            set.remove(max_cur);
-        }
+
+    boolean[] men_vis;
+    int ans = 0;
+
+    public int maxCompatibilitySum(int[][] students, int[][] mentors) {
+        int len = students.length;
+
+        men_vis = new boolean[len];
+        dfs(students, 0, mentors, 0, 0);
         return ans;
     }
 
+    void dfs(int[][] students, int stu_idx, int[][] mentors, int count, int sum) {
+        if (count >= students.length) {
+            ans = Math.max(ans, sum);
+            return;
+        }
+
+        for (int j = 0; j < mentors.length; j++) {
+            if (men_vis[j]) continue;
+            men_vis[j] = true;
+            int cc = 0;
+            for (int k = 0; k < mentors[j].length; k++) {
+                if (students[stu_idx][k] == mentors[j][k]) cc++;
+            }
+            dfs(students, stu_idx + 1, mentors, count + 1, sum + cc);
+            men_vis[j] = false;
+        }
+    }
+
     public static void main(String[] args) {
-        int[][] s={
-                {0,0,1,1,1,0,1},{0,1,1,0,0,0,0},
-        {0,0,1,1,1,1,1},{0,1,0,0,1,0,1},
-            {1,0,1,1,1,1,1}
+        int[][] s = {
+                {0, 0, 1, 1, 1, 0, 1}, {0, 1, 1, 0, 0, 0, 0},
+                {0, 0, 1, 1, 1, 1, 1}, {0, 1, 0, 0, 1, 0, 1},
+                {1, 0, 1, 1, 1, 1, 1}
+                // {1, 1, 0}, {1, 0, 1}, {0, 0, 1}
                 // {0,0},{0,0},{0,0}
         };
-        int[][] m={
-                // {1,0,0},{0,0,1},{1,1,0}
+        int[][] m = {
+                // {1, 0, 0}, {0, 0, 1}, {1, 1, 0}
                 // {1,1},{1,1},{1,1}
-                {0,1,1,0,0,0,0},{0,1,0,0,0,0,1},
-                {0,1,0,1,0,0,1},{1,0,0,0,1,0,1},
-                {1,1,1,1,1,0,0}
+                {0, 1, 1, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0, 1},
+                {0, 1, 0, 1, 0, 0, 1}, {1, 0, 0, 0, 1, 0, 1},
+                {1, 1, 1, 1, 1, 0, 0}
         };
-        System.out.println(maxCompatibilitySum(s,m));
+        System.out.println(new MaxCompatibilitySum_n5825().maxCompatibilitySum(s, m));
     }
 }
