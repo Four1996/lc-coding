60fe7556d615b99f5e74cc551161569320c2b858
panhao <phnobugs@gmail.com>
2021-07-27 21:39:08 +0800

    更新 dp背包：分割等和子集

--- dp/CanPartition_n416.java
+++ dp/CanPartition_n416.java
@@ -1,32 +1,36 @@
 package leetcode.dp;
 
-import java.util.Arrays;
+
 
 /**
- * @author panhao
+ * @author panhao 背包
  * @date 2021年06月14日 3:10 下午
  */
 public class CanPartition_n416 {
-    public boolean canPartition(int[] nums) {
+    public static boolean canPartition(int[] nums) {
         int sum = 0;
         for (int num : nums) {
             sum += num;
         }
+        int len = nums.length;
         if (sum % 2 != 0) return false;
-        boolean[][] dp = new boolean[nums.length][(sum / 2) + 1];
-        if (nums[0] <= (sum / 2)) dp[0][nums[0]] = true;
-        for (int i = 1; i < nums.length; i++) {
+        boolean[][] dp = new boolean[len + 1][sum / 2 + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= len; i++) {
             for (int j = 0; j <= sum / 2; j++) {
                 dp[i][j] = dp[i - 1][j];
-                if (nums[i] == j) {
-                    dp[i][j] = true;
+                if (nums[i - 1] <= j) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j - nums[i - 1]];
                 }
-                if (nums[i] < j) {
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
-                }
-                if (dp[nums.length - 1][sum / 2]) break;
+                if (dp[i][sum / 2]) return true;
             }
         }
-        return dp[nums.length - 1][sum / 2];
+        return dp[len - 1][sum / 2];
     }
+
+    public static void main(String[] args) {
+        int[] nums={1,5,11,5};
+        System.out.println(canPartition(nums));
+    }
+
 }
