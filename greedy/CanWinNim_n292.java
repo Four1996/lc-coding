e1d7fda682c96c91618d27372ed0dd7f51632174
panhao <phnobugs@gmail.com>
2021-09-18 10:24:35 +0800

    更新每日一题： 1、Nim游戏

---
+++ greedy/CanWinNim_n292.java
@@ -0,0 +1,13 @@
+package leetcode.greedy;
+
+/**
+ * @program: first
+ * @description: Nim游戏
+ * @author: panhao
+ * @date: 2021-09-18 09:56
+ **/
+public class CanWinNim_n292 {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
