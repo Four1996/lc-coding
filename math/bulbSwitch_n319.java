064bdb27f67015d0eb0e2764d6c3912d8f8305c5
panhao <phnobugs@gmail.com>
2021-11-15 21:44:26 +0800

    更新每日一题： 1、灯泡开关

---
+++ math/bulbSwitch_n319.java
@@ -0,0 +1,13 @@
+package leetcode.math;
+
+/**
+ * @program: first
+ * @description: 灯泡开关
+ * @author: panhao
+ * @date: 2021-11-15 21:44
+ **/
+public class bulbSwitch_n319 {
+    public int bulbSwitch(int n) {
+        return (int)Math.sqrt(n);
+    }
+}
